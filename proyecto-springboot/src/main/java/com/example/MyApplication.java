5aa7479fcfc8a9f814a0e66a8b8adecc13b6193a
Jorge Gallardo <jorgito899@gmail.com>
2025-07-14 17:12:25 -0700

    Se agrega proyecto springboot

---
+++ proyecto-springboot/src/main/java/com/example/MyApplication.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyApplication.class);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello, Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
